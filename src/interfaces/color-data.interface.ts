ae99898de43252b29eff47b8ed41aa18296af8ae
Slavko Mihajlovic <143391636+dynimorius@users.noreply.github.com>
2024-02-12 11:09:05 +0100

    Cleanup

--- src/interfaces/color-data.interface.ts
+++ src/interfaces/color-data.interface.ts
@@ -82,54 +82,6 @@ export interface ColorData {
   ypbpr?: YPbPr;
 }
 
-// export interface BasicData {
-//   cmy?: CMY;
-//   cmyk?: CMYK;
-//   hcy?: HCY;
-//   hex?: string;
-//   hsi?: HSI;
-//   hsl?: HSL;
-//   hsv?: HSV;
-//   hunter_lab?: LAB;
-//   hwb?: HWB;
-//   lab?: LAB;
-//   lch_ab?: LCH;
-//   lch_uv?: LCH;
-//   luv?: LUV;
-//   rgb?: RGB;
-//   ryb?: RYB;
-//   tsl?: TSL;
-// }
-
-// export interface ExtendedData {
-//   adobe_98_rgb?: RGB;
-//   apple_rgb?: RGB;
-//   ansi16?: number;
-//   ansi256?: number;
-//   best_rgb?: RGB;
-//   beta_rgb?: RGB;
-//   bruce_rgb?: RGB;
-//   cie_rgb?: RGB;
-//   color_match_rgb?: RGB;
-//   don_rgb_4?: RGB;
-//   eci_rgb_v2?: RGB;
-//   etka_space_ps5?: RGB;
-//   ntsc_rgb?: RGB;
-//   pal_secam_rgb?: RGB;
-//   pro_photo_rgb?: RGB;
-//   smpte_c_rgb?: RGB;
-//   wide_gamut_rgb?: RGB;
-//   uvw?: UVW;
-//   xvycc?: xvYCC;
-//   xyy?: XYY;
-//   ycbcr?: YCbCr;
-//   yccbccrc?: YcCbcCrc;
-//   ycocg?: YCoCg;
-//   ydbdr?: YDbDr;
-//   yiq?: YIQ;
-//   ypbpr?: YPbPr;
-// }
-
 
 
 
