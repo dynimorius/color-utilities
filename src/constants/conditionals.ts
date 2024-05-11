47b6e06d44787cb004ea40e62302d3ea1d28c3da
Slavko Mihajlovic <143391636+dynimorius@users.noreply.github.com>
2024-05-11 11:57:21 +0200

    added types to constants

--- src/constants/conditionals.ts
+++ src/constants/conditionals.ts
@@ -6,29 +6,29 @@
  * found at https://www.isc.org/licenses/
  */
 
-export const MAX_DECIMALS = 6;
+export const MAX_DECIMALS: number = 6;
 
 //Actual CIE standard
-export const CIE_ϵ = 0.008856;
-export const CIE_κ = 903.3;
+export const CIE_ϵ: number = 0.008856;
+export const CIE_κ: number = 903.3;
 
 //ITU-R BT.2020-2
-export const Pb = 0.791;
-export const Pr = 0.4969;
-export const Nb = -0.9702;
-export const Nr = -0.8591;
+export const Pb: number = 0.791;
+export const Pr: number = 0.4969;
+export const Nb: number = -0.9702;
+export const Nr: number = -0.8591;
 
 //ITU-R BT.2020
-export const Kr = 0.2627;
-export const Kg = 0.678;
-export const Kb = 0.0593;
+export const Kr: number = 0.2627;
+export const Kg: number = 0.678;
+export const Kb: number = 0.0593;
 
-export const NOT_PERCEPTIBLE_BY_HUMAN_EYE = 1;
-export const PRECEPTABLE_THROUGH_CLOSE_OBESERVATION = 2;
-export const PRECEPTABLE_AT_A_GLANCE = 10;
-export const MORE_CLOSE_THEN_OPPOSITE = 49;
-export const EXACT_OPPOSITE = 100;
+export const NOT_PERCEPTIBLE_BY_HUMAN_EYE: number = 1;
+export const PRECEPTABLE_THROUGH_CLOSE_OBESERVATION: number = 2;
+export const PRECEPTABLE_AT_A_GLANCE: number = 10;
+export const MORE_CLOSE_THEN_OPPOSITE: number = 49;
+export const EXACT_OPPOSITE: number = 100;
 
-export const SRGB_INVERSE_NORMALIZED_BELOW = 0.04045;
-export const SRGB_NORMALIZED_BELOW = 0.0031308;
-export const L_INVERSE_NORMALIZED_BELOW = 0.08;
+export const SRGB_INVERSE_NORMALIZED_BELOW: number = 0.04045;
+export const SRGB_NORMALIZED_BELOW: number = 0.0031308;
+export const L_INVERSE_NORMALIZED_BELOW: number = 0.08;
